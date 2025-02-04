0dd04ca6c777e50c1d59011944c62193eacbb60b
Marta Carlos <101343976+OS-martacarlos@users.noreply.github.com>
2025-02-04 14:59:03 +0000

    chore(js): fix mimeType for javascript texts
    Co-authored-by: Pedro Bilro <pedro.gustavo.bilro@outsystems.com>

--- packages/outsystems-wrapper/src/legacyCordova.ts
+++ packages/outsystems-wrapper/src/legacyCordova.ts
@@ -203,7 +203,7 @@ class LegacyCordovaBridge {
             'html': 'text/html',
             'htm': 'text/html',
             'css': 'text/css',
-            'js': 'application/javascript',
+            'js': 'text/javascript',
             'json': 'application/json',
             'png': 'image/png',
             'jpg': 'image/jpeg',
